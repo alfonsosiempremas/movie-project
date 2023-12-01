76731fdbb52cf09218cd2ced19a5c1694c366f56
alfonsosiempremas <142181536+alfonsosiempremas@users.noreply.github.com>
2023-12-01 10:57:48 -0300

    Initial files v1.0.0

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const server = require("./src/app");
 const { connection } = require("./src/db");
 const { PORT } = process.env;
 
-connection.sync({ force: true }).then(() => {
+connection.sync({ force: false }).then(() => {
     server.listen(PORT, () => {
         console.log(`Server is listening on port: ${PORT}`);
     });
